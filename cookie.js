68499456ed37a688a6581156920a3088bd0f71a4
chenag <23110134@qq.com>
2016-12-09 01:26:40 +0800

    m

--- cookie.js
+++ cookie.js
@@ -1,12 +1,10 @@
-/**
- * Created by cag on 2016/12/8.
- */
 var koa = require('koa');
 var app = koa();
 
 app.use(function *() {
     console.log(111);
-    this.cookies.set('name','CAG');
+    app.keys = ['1111','222'];
+    this.cookies.set('name','aaaaaaaaaaaa',{ signed: true });
     this.body = 'hello koa333333!';
 });
 
---
+++ generator2.js
@@ -0,0 +1,18 @@
+/**
+ * Created by cag on 2016/12/9.
+ */
+function* sayHello() {
+    var first, second;
+    yield first = '111';
+    yield second = '222';
+    yield third = '333';
+}
+var say = sayHello();
+console.log(1);
+console.log('第一个yield的值：' + say.next().done); // 111 next()调用只会执行yield后面的表达式，下一次调用时,才会执行完yield所在行的表达式。
+console.log(2);
+console.log('是否结束：' + say.next().done); // false
+console.log(3);
+console.log('是否结束：' + say.next().done); // false
+console.log(4);
+console.log('是否结束：' + say.next().done); // true 说明：next()调用只会执行yield后面的表达式,下一次调用时,才会执行完yield所在行的表达式。
