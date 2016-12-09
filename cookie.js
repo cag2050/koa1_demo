252e95af10a350d870eafadf5ba7de035cc1571b
chenanguo <chenanguo@le.com>
2016-12-09 13:49:43 +0800

    m

--- cookie.js
+++ cookie.js
@@ -3,8 +3,8 @@ var app = koa();
 
 app.use(function *() {
     console.log(111);
-    app.keys = ['1111','222'];
-    this.cookies.set('name','aaaaaaaaaaaa',{ signed: true });
+    app.keys = ['1111', '222'];
+    this.cookies.set('name', 'aaaaaaaaaaaa', {signed: true});
     this.body = 'hello koa333333!';
 });
 
