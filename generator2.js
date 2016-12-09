d4724632209c9bedfd38877b9bfa3112ff551709
chenanguo <chenanguo@le.com>
2016-12-09 09:57:11 +0800

    m

--- generator2.js
+++ generator2.js
@@ -9,9 +9,9 @@ function* sayHello() {
 }
 var say = sayHello();
 console.log(1);
-console.log('第一个yield的值：' + say.next().done); // 111 next()调用只会执行yield后面的表达式，下一次调用时,才会执行完yield所在行的表达式。
+console.log('第一个yield的值：' + say.next().value); // 111 next()调用只会执行yield后面的表达式，下一次调用时,才会执行完yield所在行的表达式。
 console.log(2);
-console.log('是否结束：' + say.next().done); // false
+console.log('第一个yield的值：' + say.next().value); // 222
 console.log(3);
 console.log('是否结束：' + say.next().done); // false
 console.log(4);
